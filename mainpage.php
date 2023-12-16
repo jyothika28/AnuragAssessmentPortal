714b0a6baf09b39979c2bf5a0de55833ceca6c9b
jyothika28 <jyothikajalla032@gmail.com>
2023-12-15 23:03:53 -0500

    Initial comit

---
+++ mainpage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="studentlog.css">
+<div id="main">
+        <img src="AUlogo.png" alt="Anurag" width="200" height="50">
+        <h1>ANURAG ASSESSMENT PORTAL </h1>
+        <div class="dropdown" >
+        <button class="login" type="button" >LOGIN</button>
+        <div class="dropdown-content">
+            <a href="index.php">Student Login</a>
+            <a href="adminpanel/admin/">Admin Login</a>
+          </div>
+        </div>
+    </div>
+<hr>
+<link rel="stylesheet" href="studentlog.css">
+<image class="bg" src="anuragbuild.jpeg"></image>
+
+<div class="text-block" >
+    <h2>SUBMIT WITH A SINGLE CLICK !</h2>
+    <p>This platform allows you to take your exams at the comfort of your home! </p>
+</div>
+
+</html>
